ca897151da298e47fc5c4ecc85c1ac1893ddcb96
Cocdrilo <alejandrolana1@gmail.com>
2024-12-17 12:34:19 +0100

    commit parcial

--- resolvers.ts
+++ resolvers.ts
@@ -1,11 +1,29 @@
-import {Collection} from "mongodb"
+import {Pokemon} from "./types.ts";
+
+const PokeApi_url = "https://pokeapi.co/api/v2/"
+
+const fetchFromPokeAPI = async (endpoint: string) => {
+    const response = await fetch(`${PokeApi_url}/${endpoint}`);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch ${endpoint}: ${response.statusText}`);
+    }
+    return response.json();
+};
 
 
 export const resolvers = {
     Query:{
+        getPokemon: async (_:unknown,args:{id:number,name:string})=>{
+            return await fetchFromPokeAPI(`pokemon/${args.id || args.name}`);
+        }
     },
 
-    Mutations:{
+    AbilitySlot: {
+        ability: async (parent: any) => await fetchFromPokeAPI(`ability/${parent.ability.name}`),
+    },
+
+    Ability: {
+        url: async (parent: any) => await fetchFromPokeAPI(parent.url)
     },
 
 
--- schema.ts
+++ schema.ts
@@ -1,27 +1,116 @@
 
 export const schema = `#graphql
-
-type Pokemon {
-    id: ID!
-    name: String!
-    abilities: [Ability]
-    moves: [Move]
-}
-
 type Ability {
     name: String!
-    effect: String
+    url: String!
+}
+
+type AbilitySlot {
+    ability: Ability!
+    is_hidden: Boolean!
+    slot: Int!
+}
+
+type Cry {
+    latest: String!
+    legacy: String!
+}
+
+type Form {
+    name: String!
+    url: String!
+}
+
+type GameIndex {
+    game_index: Int!
+    version: Version!
+}
+
+type Version {
+    name: String!
+    url: String!
 }
 
 type Move {
     name: String!
-    power: Int
+    url: String!
 }
 
+type MoveVersionDetail {
+    version_group: Version!
+    level_learned_at: Int!
+    move_learn_method: MoveLearnMethod!
+}
+
+type MoveLearnMethod {
+    name: String!
+    url: String!
+}
+
+type MoveSlot {
+    move: Move!
+    version_group_details: [MoveVersionDetail!]!
+}
+
+type PokemonSpecies {
+    name: String!
+    url: String!
+}
+
+type Sprite {
+    back_default: String
+    back_female: String
+    back_shiny: String
+    back_shiny_female: String
+    front_default: String
+    front_female: String
+    front_shiny: String
+    front_shiny_female: String
+}
+
+type Stat {
+    name: String!
+    url: String!
+}
+
+type StatSlot {
+    base_stat: Int!
+    effort: Int!
+    stat: Stat!
+}
+
+type Type {
+    name: String!
+    url: String!
+}
+
+type TypeSlot {
+    slot: Int!
+    type: Type!
+}
+
+type Pokemon {
+    id: Int!
+    name: String!
+    base_experience: Int!
+    height: Int!
+    is_default: Boolean!
+    order: Int!
+    weight: Int!
+    abilities: [AbilitySlot!]!
+    cries: Cry!
+    forms: [Form!]!
+    game_indices: [GameIndex!]!
+    held_items: [String!]!
+    location_area_encounters: String!
+    moves: [MoveSlot!]!
+    species: PokemonSpecies!
+    sprites: Sprite!
+    stats: [StatSlot!]!
+    types: [TypeSlot!]!
+}
 
 type Query {
-    pokemon(id : ID!) : Pokemon
+    getPokemon(id: Int,name:String): Pokemon
 }
-
-
 `
